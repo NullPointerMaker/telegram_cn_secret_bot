3bd12c26361f054183807f507101188c3ab330d2
NullPointerMaker <76866070+NullPointerMaker@users.noreply.github.com>
2021-06-29 22:45:14 +0800

    Optimize bash launcher

--- telegram-cn-secret-bot.bash
+++ telegram-cn-secret-bot.bash
@@ -1,39 +1,53 @@
 #!/bin/bash
-cd `dirname ${BASH_SOURCE}`
-name=`basename $BASH_SOURCE | cut -d . -f1`
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+name=$(basename "${BASH_SOURCE[0]}")
+name=${name%.*}
 command="python3 -u secret.py"
-function doStart {
-	pid=`pidof $name`
-	if [ ! $pid ] ; then
-		rm $name.out $name.err
-		bash -c "exec -a $name $command &" 1>>$name.out 2>>$name.err
-	fi
-	pid=`pidof $name`
-	echo $pid started
+function doStart() {
+  pid=$(pidof "$name")
+  if [ ! "$pid" ]; then
+    rm "$name.out" "$name.err"
+    bash -c "exec -a $name $command &" 1>>"$name.out" 2>>"$name.err"
+  fi
+  sleep 1
+  pid=$(pidof "$name")
+  if [ "$pid" ]; then
+    echo "$pid started"
+  else
+    cat "$name.out" "$name.err"
+    exit 10
+  fi
 }
-function doStop {
-	pid=`pidof $name`
-	if [ $pid ] ; then
-		echo $pid stopped
-		kill $pid
-	fi
+
+function doStop() {
+  pid=$(pidof "$name")
+  # shellcheck disable=SC2086
+  if [ "$pid" ]; then
+    kill $pid
+    while kill -0 $pid 2>/dev/null; do
+      sleep 1
+    done
+    echo "$pid stopped"
+  fi
 }
+
 trap 'onCtrlC' INT
-function onCtrlC {
-	doStop
+
+function onCtrlC() {
+  doStop
 }
-if [ "$1" == "stop" ] ; then
-	doStop
-	exit
-elif [ "$1" == "start" ] ; then
-	doStart
-	exit
-elif [ "$1" == "restart" ] ; then
-	doStop
-	sleep 1
-	doStart
-	exit
+
+if [ "$1" == "stop" ]; then
+  doStop
+  exit
+elif [ "$1" == "start" ]; then
+  doStart
+  exit
+elif [ "$1" == "restart" ]; then
+  doStop
+  doStart
+  exit
 else
-	doStart
-	tail --pid=$pid -qf $name.out $name.err
+  doStart
+  tail --pid="$pid" -qf "$name.out" "$name.err"
 fi
